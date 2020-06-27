6e1453b0b08b427a51ff879449207b1e48325f59
eminmugla09 <64754780+eminmugla09@users.noreply.github.com>
2020-06-27 16:03:32 -0400

    Update SmartHome.js

--- navigation/SmartHome.js
+++ navigation/SmartHome.js
@@ -2,9 +2,9 @@ import React from "react";
 import { createStackNavigator } from "react-navigation-stack";
 
 import Dashboard from "../screens/Dashboard";
-import WatchPage from "../screens/WatchPage";
+import ClockPage from "../screens/ClockPage";
 import TimerPage from "../screens/TimerPage";
-import GamePage from "../screens/GamePage";
+import AlarmPage from "../screens/AlarmPage";
 import WeatherPage from "../screens/WeatherPage";
 import AnimationPage from "../screens/AnimationPage";
 import Settings from "../screens/Settings";
@@ -15,9 +15,9 @@ export default createStackNavigator(
   {
     Dashboard,
     Settings,
-    WatchPage,
+    ClockPage,
     TimerPage,
-    GamePage,
+    AlarmPage,
     WeatherPage,
     AnimationPage
     
